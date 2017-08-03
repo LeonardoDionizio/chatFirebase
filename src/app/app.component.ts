1f4852b34d94ca2ba7ec0972b7dda8962b1ac8a9
Leonardo Dionizio <leodionizio13@gmail.com>
2017-08-03 17:13:17 -0300

    Configurações de estilos e de login com firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import {
+  AngularFireDatabase,
+  FirebaseListObservable
+} from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,85 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  items: FirebaseListObservable<any>;
+  provider = new firebase.auth.FacebookAuthProvider();
+  name: any;
+  msgVal = '';
+
+  constructor(public af: AngularFireDatabase, private afAuth: AngularFireAuth) {
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 5
+      }
+    });
+    this.afAuth.authState.subscribe(auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    });
+  }
+
+  login() {
+    firebase
+      .auth()
+      .signInWithPopup(this.provider)
+      .then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const token = result.credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        const errorCode = error.name;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        // const email = error.email;
+        // // The firebase.auth.AuthCredential type that was used.
+        // const credential = .credential;
+        // ...
+      });
+
+    // firebase
+    //   .auth()
+    //   .signInWithRedirect(new firebase.auth.FacebookAuthProvider())
+    //   .then(function(result) {
+    //     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    //     const token = result.credential.accessToken;
+    //     // The signed-in user info.
+    //     const user = result.user;
+    //     // ...
+    //   })
+    //   .catch(function(error) {
+    //     // Handle Errors here.
+    //     // const errorCode = error.code;
+    //     console.log('erro');
+    //     const errorMessage = error.message;
+    //     // The email of the user's account used.
+    //     // const email = error.email;
+    //     // The firebase.auth.AuthCredential type that was used.
+    //     // const credential = error.credential;
+    //     // ...
+    //   });
+  }
+
+  chatSend(theirMessage: string) {
+    this.items.push({
+      message: theirMessage,
+      name: this.name.displayName
+    });
+    this.msgVal = '';
+  }
+
+  logout() {
+    firebase.auth().signOut().then(
+      function() {
+        // Sign-out successful.
+      },
+      function(error) {
+        // An error happened.
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBSPDqsWRkuGEK7Q7_fiMA7ZOSj8om2x3M',
+  authDomain: 'chatapp-c8f50.firebaseapp.com',
+  databaseURL: 'https://chatapp-c8f50.firebaseio.com',
+  projectId: 'chatapp-c8f50',
+  storageBucket: 'chatapp-c8f50.appspot.com',
+  messagingSenderId: '1037362870910'
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
